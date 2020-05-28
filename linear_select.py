f9a9e3394a910f95759bb426978a0fb3847f3224
Lucia Zheng <zlucia@stanford.edu>
2020-05-27 19:37:29 -0700

    Experiments for optimized linear selection on different workflows

--- linear_select.py
+++ linear_select.py
@@ -16,7 +16,7 @@ def partition(pivot, lst):
 
 	return (L, R)
 
-def select(k, lst, tune_dmcs, tune_eps):
+def select(k, lst, method):
 	n = len(lst)
 	if k > n or n < 1:
 		raise Exception('Invalid k value')
@@ -30,33 +30,32 @@ def select(k, lst, tune_dmcs, tune_eps):
 	# eps = 0
 
 	# Simple, using Chazelle's constant value for eps
-	if not tune_dmcs and not tune_eps:
+	if method == 1:
 		delete_min_calls = max(1, math.floor(n/3))
 		eps = 1/3
 	# Choose delete_min_calls and eps in ways that leverage soft heap corruption properties
 	# to speed up select algorithm
-	elif not tune_dmcs:
+	elif method == 2:
 		# Holds delete_min_calls constant, varies eps
 		r = k/n
 		delete_min_calls = max(1, math.floor(n/3))
-		if r > 1/3:
+		if r >= 1/3:
 			eps = r - 1/3
 		else:
 			eps = 1/10
-	else:
+	elif method == 3:
 		# Varies delete_min_calls and eps
 		r = k/n
 		if r >= 2/3:
 			delete_min_calls = max(1, math.floor(2 * n/3))
 			eps = r - 2/3
-			# delete_min_calls = max(1, math.floor(k/2))
-			# eps = r - ((k/2)/n)
 		elif r >= 1/3:
 			delete_min_calls = max(1, math.floor(n/3))
 			eps = r - 1/3
 		else:
 			delete_min_calls = k
 			eps = r
+	# print(n)
 
 	sheap = SoftHeap(eps)
 	for elem in lst:
@@ -75,22 +74,21 @@ def select(k, lst, tune_dmcs, tune_eps):
 	if len(L) == k - 1:
 		return max_seen
 	elif len(L) >= k:
-		return select(k, L, tune_dmcs, tune_eps)
+		return select(k, L, method)
 	else:
-		return select(k - len(L) - 1, R, tune_dmcs, tune_eps)
+		return select(k - len(L) - 1, R, method)
 
 # Run experiment on select k execution time for different values of k on t lists of size 10000
 # Using three tuning methods for choosing delete_min calls/corruption parameter within select k
-def run_exp1():
+def run_exp1(t):
 	ks = list(range(0, 10001, 1000))
 	ks[0] = 1
 	
 	data = {'x': ks, 'y1': [], 'y2': [], 'y3': []}
 	
-	t = 1
-	# Average execution time over t random permutations/orderings of a list of numbers [1,...,1000]
+	# Average execution time over t random permutations/orderings of a list of size 10000
 	for i in range(t):
-		# print('i', i)
+		print('i', i)
 		lst = np.random.permutation(list(range(1, 10001)))
 
 		y1 = []
@@ -98,13 +96,13 @@ def run_exp1():
 		y3 = []
 
 		for k in ks:
-			# print('k', k)
-			# Average execution time of select k on lst over 100 runs
-			t = timeit.timeit(functools.partial(select, k, lst, False, False), number=100)/100
+			print('k', k)
+			# Average execution time of select k on lst over 100 run
+			t = timeit.timeit(functools.partial(select, k, lst, 1), number=10)/10
 			y1.append(t)
-			t = timeit.timeit(functools.partial(select, k, lst, False, True), number=100)/100
+			t = timeit.timeit(functools.partial(select, k, lst, 2), number=10)/10
 			y2.append(t)
-			t = timeit.timeit(functools.partial(select, k, lst, True, True), number=100)/100
+			t = timeit.timeit(functools.partial(select, k, lst, 3), number=10)/10
 			y3.append(t)
 
 		data['y1'].append(y1)
@@ -118,7 +116,7 @@ def run_exp1():
 
 	return data
 
-def make_plot1(data):
+def make_plot1(data, filename):
 	fig, ax = plt.subplots()
 	for i in range(1, 4):
 		ax.plot(data['x'], data['y' + str(i)])
@@ -126,56 +124,79 @@ def make_plot1(data):
 	ax.set_ylabel('Average Execution Time of Select (seconds)')
 	ax.set_title('Average Execution Time of Select (seconds)')
 	lgd = plt.legend(['No tuning', 'Tuning eps', 'Tuning delete_min calls and eps'], title='Tuning Method', loc='center left', bbox_to_anchor=(1, 0.5))
-	plt.savefig('exps1.png', bbox_extra_artists=(lgd,), bbox_inches='tight', format='png')
+	plt.savefig(filename, bbox_extra_artists=(lgd,), bbox_inches='tight', format='png')
 
-# Run experiment on select median execution time for different list sizes
+# Run experiment on select k execution time for different list sizes (defaults to selecting minimum)
 # Using three tuning methods for choosing delete_min calls/corruption parameter within select k
-def run_exp2():
-	lst_sizes = list(range(1000, 10001, 1000))
+def run_exp2(p=None):
+	lst_sizes = list(range(0, 10001, 1000))
+	lst_sizes[0] = 1
 	
 	data = {'x': lst_sizes, 'y1': [], 'y2': [], 'y3': []}
 	
 	for n in lst_sizes:
 		lst = np.random.permutation(list(range(1, n + 1)))
-		k = n//2
-		#print(n, k)
+		if not p:
+			k = 1
+		else:
+			k = math.ceil(n * p)
+		print(n, k)
 
-		t = timeit.timeit(functools.partial(select, k, lst, False, False), number=100)/100
+		t = timeit.timeit(functools.partial(select, k, lst, 1), number=10)/10
 		data['y1'].append(t)
-		t = timeit.timeit(functools.partial(select, k, lst, False, True), number=100)/100
+		t = timeit.timeit(functools.partial(select, k, lst, 2), number=10)/10
 		data['y2'].append(t)
-		t = timeit.timeit(functools.partial(select, k, lst, True, True), number=100)/100
+		t = timeit.timeit(functools.partial(select, k, lst, 3), number=10)/10
 		data['y3'].append(t)
 
 	return data
 
-def make_plot2(data):
+def make_plot2(data, title, filename):
 	fig, ax = plt.subplots()
 	for i in range(1, 4):
 		ax.plot(data['x'], data['y' + str(i)])
 	ax.set_xlabel('List size - n')
-	ax.set_ylabel('Average Execution Time of Select Median (seconds)')
-	ax.set_title('Average Execution Time of Select Median (seconds)')
+	ax.set_ylabel(title)
+	ax.set_title(title)
 	lgd = plt.legend(['No tuning', 'Tuning eps', 'Tuning delete_min calls and eps'], title='Tuning Method', loc='center left', bbox_to_anchor=(1, 0.5))
-	plt.savefig('exps2.png', bbox_extra_artists=(lgd,), bbox_inches='tight', format='png')
+	plt.savefig(filename, bbox_extra_artists=(lgd,), bbox_inches='tight', format='png')
 
 def main():
-	# Sanity check that select is working correctly
-	k = 1
-	lst = np.random.permutation(list(range(1, 10001)))
-	print('No tuning')
-	print(select(k, lst, False, False))
-	print('Tuning eps')
-	print(select(k, lst, False, True))
-	print('Tuning delete_min calls and eps')
-	print(select(k, lst, True, True))
+	# # Sanity check that select is working correctly
+	# k = 1
+	# lst = np.random.permutation(list(range(1, 10001)))
+	# # lst = list(range(1, 10001))
+	# print('No tuning')
+	# # print(select(k, lst, 1))
+	# t = timeit.timeit(functools.partial(select, k, lst, 1), number=10)/10
+	# print(t)
+	# print('Tuning eps')
+	# # print(select(k, lst, 2))
+	# t = timeit.timeit(functools.partial(select, k, lst, 2), number=10)/10
+	# print(t)
+	# print('Tuning delete_min calls and eps')
+	# # print(select(k, lst, 3))
+	# t = timeit.timeit(functools.partial(select, k, lst, 3), number=10)/10
+	# print(t)
 
-	# # Runs experiment on select k, varying methods for choosing selection parameters affecting the pivot
-	# data = run_exp1()
-	# make_plot1(data)
+	# Runs experiment on select k, varying methods for choosing selection parameters affecting the pivot
+	data = run_exp1(t=1)
+	make_plot1(data, 'exp1.png')
 
-	# data = run_exp2()
-	# make_plot2(data)
+	data = run_exp2()
+	make_plot2(data, 'Average Execution Time of Select for Minimum Element (seconds)', 'exp2_min.png')
+
+	data = run_exp2(p=1/3)
+	make_plot2(data, 'Average Execution Time of Select for Rank n/3 Element (seconds)', 'exp2_third.png')
+
+	data = run_exp2(p=1/2)
+	make_plot2(data, 'Average Execution Time of Select for Median Element (seconds)', 'exp2_median.png')
+
+	data = run_exp2(p=2/3)
+	make_plot2(data, 'Average Execution Time of Select for Rank 2n/3 Element (seconds)', 'exp2_twothirds.png')
+
+	data = run_exp2(p=1)
+	make_plot2(data, 'Average Execution Time of Select for Maximum Element (seconds)', 'exp2_max.png')
 
 if __name__ == '__main__':
 	main()
